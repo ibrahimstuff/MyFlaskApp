4fd5c671cc8fbada873105563d21835c8a83117d
Mohammed Ibrahimali <tech.ibrahim@outlook.com>
2018-05-20 20:04:51 +0530

    update the articles and the setuped for mysql

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -9,6 +15,14 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Article One',
+            'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium enim placeat at magnam voluptatibus eligendi blanditiis itaque error soluta. Magnam rerum fugiat voluptates quidem atque non id est laborum in.',
+            'author' : 'Ibrahim',
+            'create_date': '04-25-2018'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article Two',
+            'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium enim placeat at magnam voluptatibus eligendi blanditiis itaque error soluta. Magnam rerum fugiat voluptates quidem atque non id est laborum in.',
+            'author' : 'ARMIA',
+            'create_date': '04-25-2018'
+        },
+        {
+            'id' : 3,
+            'title' : 'Article Three',
+            'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium enim placeat at magnam voluptatibus eligendi blanditiis itaque error soluta. Magnam rerum fugiat voluptates quidem atque non id est laborum in.',
+            'author' : 'Ibrahim',
+            'create_date': '04-25-2018'
+        }
+        
+    ]
+    return articles
